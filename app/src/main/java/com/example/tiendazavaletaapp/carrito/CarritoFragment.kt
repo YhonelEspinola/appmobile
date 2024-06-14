5ac97acfff45ffa5f2ba3b18b9c1d32c9445d927
vhuertas <pvhuertas@cibertec.edu.pe>
2024-06-14 01:58:32 -0500

    included print

--- app/src/main/java/com/example/tiendazavaletaapp/carrito/CarritoFragment.kt
+++ app/src/main/java/com/example/tiendazavaletaapp/carrito/CarritoFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tiendazavaletaapp.R
+import com.example.tiendazavaletaapp.pagoEntrega.PagoEntregraFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CarritoFragment : Fragment(){
 
@@ -22,6 +26,7 @@ class CarritoFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val btnProcesarCompra = view.findViewById<Button>(R.id.btnProcesarCompra)
 
         val listCarrito = listOf<Carrito>(
             Carrito("La Odisea","...",12.50,2,R.drawable.logo),
@@ -37,6 +42,15 @@ class CarritoFragment : Fragment(){
         val adapterC = CarritoAdapter(listCarrito)
         recyclerCarrito.adapter= adapterC
         recyclerCarrito.layoutManager= LinearLayoutManager(context)
+
+
+        btnProcesarCompra.setOnClickListener {
+            val newFragment = PagoEntregraFragment.pagoentregaInstance()
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_menu2, newFragment)
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            transaction.commit()
+        }
     }
 
     companion object{
