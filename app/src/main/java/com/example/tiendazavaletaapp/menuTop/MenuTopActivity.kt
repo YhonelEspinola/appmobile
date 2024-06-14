ec0c7d1cd2c0705981f8e5a6f5683d8d5dccb030
vhuertas <pvhuertas@cibertec.edu.pe>
2024-06-13 23:33:46 -0500

    initial commit

---
+++ app/src/main/java/com/example/tiendazavaletaapp/menuTop/MenuTopActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tiendazavaletaapp.menuTop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiendazavaletaapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MenuTopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_top)
+
+
+
+    }
+}
