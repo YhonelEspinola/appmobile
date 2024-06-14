a4aa7d3e7ae23487051b3f18cd9d95966860525f
vhuertas <pvhuertas@cibertec.edu.pe>
2024-06-14 01:29:42 -0500

    included print

--- app/src/main/java/com/example/tiendazavaletaapp/menuTop/MenuTopActivity.kt
+++ app/src/main/java/com/example/tiendazavaletaapp/menuTop/MenuTopActivity.kt
@@ -2,7 +2,10 @@ package com.example.tiendazavaletaapp.menuTop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.tiendazavaletaapp.R
+import com.example.tiendazavaletaapp.pagoEntrega.PagoEntregraFragment
+import com.example.tiendazavaletaapp.recojoTienda.RecojoTiendaFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -11,7 +14,29 @@ class MenuTopActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_top)
 
+        val nav_view2 =findViewById<BottomNavigationView>(R.id.nav_view2)
+        nav_view2.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.itemPagoEntrega -> {
+                    val fragment =PagoEntregraFragment.pagoentregaInstance()
+                    openFregment(fragment)
+                    true
+                }
+                R.id.itemRecojoTienda -> {
+                    val fragment = RecojoTiendaFragment.recojoInstance()
+                    openFregment(fragment)
+                    true
+                }
+                else -> false
+            }
+        }
 
+        nav_view2.selectedItemId = R.id.itemPagoEntrega
+    }
 
+    fun openFregment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_menu2, fragment)
+        transaction.commit()
     }
 }
