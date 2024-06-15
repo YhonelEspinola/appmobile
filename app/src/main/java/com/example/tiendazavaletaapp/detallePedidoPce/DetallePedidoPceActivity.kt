01d56b70a1f83c68fa77e8c70b1abb62d8c30bbd
vhuertas <pvhuertas@cibertec.edu.pe>
2024-06-15 01:14:54 -0500

    conexiones de activity detalle_pedido_pce_activity

--- app/src/main/java/com/example/tiendazavaletaapp/detallePedidoPce/DetallePedidoPceActivity.kt
+++ app/src/main/java/com/example/tiendazavaletaapp/detallePedidoPce/DetallePedidoPceActivity.kt
@@ -1,12 +1,22 @@
 package com.example.tiendazavaletaapp.detallePedidoPce
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiendazavaletaapp.R
+import com.example.tiendazavaletaapp.menu.MenuActivity
 
 class DetallePedidoPceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_pedido_pce)
+
+        val btnCancela = findViewById<Button>(R.id.btnCancela)
+
+        btnCancela.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
