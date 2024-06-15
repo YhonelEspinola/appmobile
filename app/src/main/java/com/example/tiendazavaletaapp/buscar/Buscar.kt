2eb75c72d7141e8993e5ac936c1bd2a0cb96c3d8
AdrianJyQ <adrian.garcia.alcantara.jyq@gmail.com>
2024-06-15 18:42:41 -0500

    Search fragment implement (Not working search bar)

---
+++ app/src/main/java/com/example/tiendazavaletaapp/buscar/Buscar.kt
@@ -0,0 +1,11 @@
+package com.example.tiendazavaletaapp.buscar
+
+data class Buscar (
+    val  marca:String,
+    val  titulo:String,
+    val  categoria:String,
+    val  precio: Double,
+    val  imagen:Int
+)
+
+
