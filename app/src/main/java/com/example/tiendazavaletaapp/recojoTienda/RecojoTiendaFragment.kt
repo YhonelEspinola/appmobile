6fe7f07f6595ee7312a01121187f7b24b610ec10
vhuertas <pvhuertas@cibertec.edu.pe>
2024-06-15 02:25:08 -0500

    conexion de detalle_pedido_rt

--- app/src/main/java/com/example/tiendazavaletaapp/recojoTienda/RecojoTiendaFragment.kt
+++ app/src/main/java/com/example/tiendazavaletaapp/recojoTienda/RecojoTiendaFragment.kt
@@ -1,11 +1,15 @@
 package com.example.tiendazavaletaapp.recojoTienda
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.tiendazavaletaapp.R
+import com.example.tiendazavaletaapp.detallePedidoPce.DetallePedidoPceActivity
+import com.example.tiendazavaletaapp.detallePedidoRt.DetallePedidoRtActivitty
 
 class RecojoTiendaFragment : Fragment() {
     override fun onCreateView(
@@ -18,6 +22,12 @@ class RecojoTiendaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val btnContinuarRecojo : Button = view.findViewById(R.id.btnContinuarRecojo)
+        btnContinuarRecojo.setOnClickListener {
+            val intent = Intent(activity, DetallePedidoRtActivitty::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object{
