6cce930d7c47e19192ccf73bae5673fd300fad59
vhuertas <pvhuertas@cibertec.edu.pe>
2024-06-15 00:55:58 -0500

    nuevo activity_detalle_pedido_pce

---
+++ app/src/main/java/com/example/tiendazavaletaapp/detallePedidoPce/DetallePedidoPceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tiendazavaletaapp.detallePedidoPce
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiendazavaletaapp.R
+
+class DetallePedidoPceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pedido_pce)
+    }
+}
