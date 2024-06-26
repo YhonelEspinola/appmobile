e8c44bb9720960e9b6f23cc9cabbcc8fd8640f22
vhuertas <pvhuertas@cibertec.edu.pe>
2024-06-25 23:43:34 -0500

    inicio sesion funcionando

--- app/src/main/java/com/example/tiendazavaletaapp/login/LoginActivity.kt
+++ app/src/main/java/com/example/tiendazavaletaapp/login/LoginActivity.kt
@@ -1,53 +1,93 @@
 package com.example.tiendazavaletaapp.login
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tiendazavaletaapp.R
+import androidx.lifecycle.ViewModelProvider
+import com.example.tiendazavaletaapp.databinding.ActivityLoginBinding
 import com.example.tiendazavaletaapp.gestionAdmid.GestionAdminActivity
-import com.example.tiendazavaletaapp.menu.MenuActivity
 import com.example.tiendazavaletaapp.recuperarContrasenia.RecuperarContraseniaActivity
 import com.example.tiendazavaletaapp.registrarUsuario.RegistrarUsuarioActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: LoginViewModel
+
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val email = findViewById<EditText>(R.id.edtEmail)
-        val password = findViewById<EditText>(R.id.edtPassword)
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val btnRegister = findViewById<TextView>(R.id.textRegistrarse)
-        val txtOlvidarContra = findViewById<TextView>(R.id.txtOlvidarContra)
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
 
-        btnLogin.setOnClickListener {
-            val emailVale = email.text.toString()
-            val passwordValue = password.text.toString()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
 
-            if (emailVale.equals("admin@gmail.com") && passwordValue.equals("123456")) {
-                val intent = Intent(this, GestionAdminActivity::class.java)
-                startActivity(intent)
-            }
-            else if(emailVale.equals("jorge@gmail.com") && passwordValue.equals("123")) {
-                val intent = Intent(this, MenuActivity::class.java)
-                startActivity(intent)
-            }
-            else {
-                Toast.makeText(this,
-                    "Verifique datos", Toast.LENGTH_SHORT).show()
-            }
+        binding.btnLogin.setOnClickListener {
+            val email = binding.textEmail.text.toString()
+            val password = binding.textPassword.text.toString()
+
+            progressDialog.setMessage("Ingresando")
+            progressDialog.show()
+
+            viewModel.validarInfo(email,password)
+
         }
-        btnRegister.setOnClickListener {
-            val intent = Intent(this, RegistrarUsuarioActivity::class.java)
-            startActivity(intent)
+        binding.textRegistrarse.setOnClickListener {
+            startActivity(Intent(applicationContext, RegistrarUsuarioActivity::class.java))
         }
-        txtOlvidarContra.setOnClickListener {
-            val intent = Intent(this, RecuperarContraseniaActivity::class.java)
-            startActivity(intent)
+        binding.txtOlvidarContra.setOnClickListener {
+            startActivity(Intent(applicationContext, RecuperarContraseniaActivity::class.java))
         }
+
+        observerLiveData()
+
     }
+
+    private fun observerLiveData(){
+        viewModel.userLoginStatus.observe(this){status ->
+            if(status){
+                progressDialog.dismiss()
+                startActivity(Intent(this,GestionAdminActivity::class.java))
+                finishAffinity()
+                Toast.makeText(this,
+                    "Bienvenido",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else{
+                progressDialog.dismiss()
+                viewModel.masajeError.value?.let {
+                    Toast.makeText(this,it,Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        viewModel.emailError.observe(this){ errorMessage ->
+            binding.textEmail.error = errorMessage
+            if (errorMessage != null){
+                binding.textEmail.requestFocus()
+            }
+        }
+
+        viewModel.passwordError.observe(this){ errorMessage ->
+            binding.textPassword.error = errorMessage
+            if (errorMessage != null){
+                binding.textPassword.requestFocus()
+            }
+        }
+
+        viewModel.masajeError.observe(this){ errorMessage ->
+            if (errorMessage != null){
+                Toast.makeText(this,errorMessage,Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/tiendazavaletaapp/login/LoginViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.tiendazavaletaapp.login
+
+import android.util.Patterns
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginViewModel: ViewModel() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    val userLoginStatus = MutableLiveData<Boolean>()
+    val emailError = MutableLiveData<String>()
+    val passwordError = MutableLiveData<String>()
+    val masajeError = MutableLiveData<String>()
+
+    fun validarInfo(email: String, password: String){
+        if (email.isEmpty()){
+            emailError.value = "Ingrese su correo"
+            userLoginStatus.value = false
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailError.value = "Correo no válido"
+            userLoginStatus.value = false
+        }else if (password.isEmpty()){
+            passwordError.value = "Ingrese su contraseña"
+            userLoginStatus.value = false
+        }else{
+            loginFirebase(email,password)
+        }
+    }
+
+    private fun loginFirebase(email : String, password : String) {
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                userLoginStatus.value = it.isSuccessful
+                if (!it.isSuccessful){
+                    masajeError.value = "Error al iniciar sesión, verifique datos"
+                }
+            }
+    }
+
+
+}
